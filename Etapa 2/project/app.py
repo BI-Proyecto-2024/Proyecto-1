b81dff8f87a3b3dd5315d62cbaf6a231aa9aac94
JuanSarS <98623401+JuanSarS@users.noreply.github.com>
2024-10-12 17:36:41 -0500

    Cambios

--- Etapa 2/project/app.py
+++ Etapa 2/project/app.py
@@ -1,28 +1,56 @@
 from flask import Flask, request, render_template
 import requests
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 API_URL = "http://127.0.0.1:8000"  # Cambia esto a la URL de tu API
 
+
+# Función para verificar extensiones de archivos permitidos
+def allowed_file(filename):
+    allowed_extensions = {'xlsx','.csv'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('indexapp.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     text = request.form.get('text')
-    response = requests.post(f"{API_URL}/predict", json={"texts": [text]})
+    opinion_file = request.files.get('opinion_file')
+    files={}
+    data={}
+    if opinion_file and allowed_file(opinion_file.filename):
+        # Guardar archivo de forma temporal
+        file_path = os.path.join('temp', secure_filename(opinion_file.filename))
+        opinion_file.save(file_path)
+        files = {'file': open(file_path, 'rb')}
+    else: 
+         data = {'text_input': text}    
+
+    response = requests.post(f"{API_URL}/predict", files=files, data=data)
     prediction = response.json()
-    return render_template('index.html', prediction=prediction)
+
+    return render_template('indexapp.html', prediction=prediction)
 
 @app.route('/retrain', methods=['POST'])
 def retrain():
-    texts = request.form.getlist('texts')  # Recoge múltiples textos
-    labels = request.form.getlist('labels')  # Recoge las etiquetas correspondientes
-    response = requests.post(f"{API_URL}/retrain", json={"texts": texts, "labels": labels})
+    opinion_file = request.files.get('opinion_file')
+    files={}
+    if opinion_file and allowed_file(opinion_file.filename):
+        # Guardar archivo de forma temporal
+        file_path = os.path.join('temp', secure_filename(opinion_file.filename))
+        opinion_file.save(file_path)
+        files = {'file': open(file_path, 'rb')}
+     
+    response = requests.post(f"{API_URL}/retrain", files=files)
     metrics = response.json()
-    return render_template('index.html', metrics=metrics)
+
+    return render_template('indexapp.html', metrics=metrics)
 
 if __name__ == '__main__':
     app.run(debug=True)
