a684e5e429b66aa3d77f331b989376232cc8616f
Maria Almeciga <malmeciga0104@gmail.com>
2024-10-12 19:21:55 -0500

    aa

--- Etapa 2/project/app.py
+++ Etapa 2/project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import requests
 import os
 from werkzeug.utils import secure_filename
@@ -19,36 +19,42 @@ def allowed_file(filename):
 def index():
     return render_template('indexapp.html')
 
+from flask import jsonify
+import traceback
+
 @app.route('/predict', methods=['POST'])
 def predict():
     text = request.form.get('text')
     opinion_file = request.files.get('opinion_file')
-    files={}
-    data={}
-    prediction= {}
+    files = {}
+    data = {}
+    
     try:
-      if opinion_file and allowed_file(opinion_file.filename):
-        # Guardar archivo de forma temporal
-        file_path = BytesIO(opinion_file.read())
-        files = {'file': open(file_path, 'rb')}
-      else: 
-         data = {'text_input': text}    
+        if opinion_file and allowed_file(opinion_file.filename):
+            # Guardar archivo de forma temporal
+            file_path = os.path.join('temp', secure_filename(opinion_file.filename))
+            opinion_file.save(file_path)
+            files = {'file': open(file_path, 'rb')}
+        else: 
+            data = {'text_input': text}    
 
-      response = requests.post(f"{API_URL}/predict", files=files, data=data)
-      prediction = response.json()
+        response = requests.post(f"{API_URL}/predict", files=files, data=data)
+        response.raise_for_status()  # Lanza un error si la respuesta es 4xx o 5xx
+        prediction = response.json()
+        return jsonify(prediction)  # Asegúrate de devolver el JSON aquí
     except Exception as e:
-           traceback.print_exc()   
-           return render_template('indexapp.html', error=str(e))
-    return prediction
+        traceback.print_exc()
+        return jsonify({'error': str(e)}), 500  # Devolver error en formato JSON
+
 
 @app.route('/retrain', methods=['POST'])
 def retrain():
-    opinion_file = request.files.get('opinion_file')
+    labels_file = request.files.get('labels_file')
     files={}
-    if opinion_file and allowed_file(opinion_file.filename):
+    if labels_file and allowed_file(labels_file.filename):
         # Guardar archivo de forma temporal
-        file_path = os.path.join('temp', secure_filename(opinion_file.filename))
-        opinion_file.save(file_path)
+        file_path = os.path.join('temp', secure_filename(labels_file.filename))
+        labels_file.save(file_path)
         files = {'file': open(file_path, 'rb')}
      
     response = requests.post(f"{API_URL}/retrain", files=files)
